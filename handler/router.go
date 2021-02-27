daf262e059e4319107ca5d6591733f7ec9579a24
themmj <themmj@users.noreply.github.com>
2021-02-27 05:35:09 +0100

    Fix formatting in handler

--- handler/router.go
+++ handler/router.go
@@ -1,60 +1,60 @@
 package handler
 
 import (
-    "github.com/gorilla/mux"
-    "net/http"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func Register(baseRouter *mux.Router) {
-    d := baseRouter.
-        PathPrefix("/driver").
-        Subrouter()
-    {
-        d.
-            Path("/start").
-            Methods(http.MethodPost).
-            HandlerFunc(StartDriverSession)
-        d.
-            Path("/pickup").
-            Methods(http.MethodPost).
-            HandlerFunc(ConfirmPickup)
-        d.
-            Path("/information").
-            Methods(http.MethodGet).
-            HandlerFunc(GetPassengerInfo)
-        d.
-            Path("/locations").
-            Methods(http.MethodPost).
-            HandlerFunc(UpdateRouteLocations)
-        d.
-            Path("/estimations").
-            Methods(http.MethodPost).
-            HandlerFunc(UpdateEsitmations)
-        d.
-            Path("/confirmations").
-            Methods(http.MethodPost).
-            HandlerFunc(ConfirmRideRequest)
-    }
+	d := baseRouter.
+		PathPrefix("/driver").
+		Subrouter()
+	{
+		d.
+			Path("/start").
+			Methods(http.MethodPost).
+			HandlerFunc(StartDriverSession)
+		d.
+			Path("/pickup").
+			Methods(http.MethodPost).
+			HandlerFunc(ConfirmPickup)
+		d.
+			Path("/information").
+			Methods(http.MethodGet).
+			HandlerFunc(GetPassengerInfo)
+		d.
+			Path("/locations").
+			Methods(http.MethodPost).
+			HandlerFunc(UpdateRouteLocations)
+		d.
+			Path("/estimations").
+			Methods(http.MethodPost).
+			HandlerFunc(UpdateEstimations)
+		d.
+			Path("/confirmations").
+			Methods(http.MethodPost).
+			HandlerFunc(ConfirmRideRequest)
+	}
 
-    p := baseRouter.
-        PathPrefix("/passenger").
-        Subrouter()
-    {
-        p.
-            Path("/start").
-            Methods(http.MethodPost).
-            HandlerFunc(StartPassengerSession)
-        p.
-            Path("/request").
-            Methods(http.MethodPost).
-            HandlerFunc(RequestRide)
-        p.
-            Path("/information").
-            Methods(http.MethodGet).
-            HandlerFunc(GetDriverInfo)
-        p.
-            Path("/location").
-            Methods(http.MethodPost).
-            HandlerFunc(UpdatePassengerLocation)
-    }
+	p := baseRouter.
+		PathPrefix("/passenger").
+		Subrouter()
+	{
+		p.
+			Path("/start").
+			Methods(http.MethodPost).
+			HandlerFunc(StartPassengerSession)
+		p.
+			Path("/request").
+			Methods(http.MethodPost).
+			HandlerFunc(RequestRide)
+		p.
+			Path("/information").
+			Methods(http.MethodGet).
+			HandlerFunc(GetDriverInfo)
+		p.
+			Path("/location").
+			Methods(http.MethodPost).
+			HandlerFunc(UpdatePassengerLocation)
+	}
 }
